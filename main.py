ff6870529f98f3bf2957c31eb3406feea9f9d0b5
taki-21 <taki.it.0208.21@gmail.com>
2022-02-21 02:13:43 +0900

    update: getメソッドの更新

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
  # @はデコレーター
-@app.get('/')
-async def index():
-    return {'message': 'Hello world'}
+@app.get('/countries/{country_name}')
+async def country(country_name):
+    return {'country_name': country_name}
