b414f06732d792d74c502a52a31cbd209194e08c
taki-21 <taki.it.0208.21@gmail.com>
2022-02-21 23:16:10 +0900

    add: パスパラメータ

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
- # @はデコレーター
 @app.get('/countries/{country_name}')
-async def country(country_name):
+async def country(country_name: str):
     return {'country_name': country_name}
+
+@app.get('/countries/japan')
+async def japan():
+    return {'message': 'This is Japan'}
