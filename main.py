155a82c5261787303a4c98b5b5dac4c7f67ddcb6
taki-21 <taki.it.0208.21@gmail.com>
2022-02-22 21:53:43 +0900

    add: クエリパラメータの追加

--- main.py
+++ main.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/countries/{country_name}')
-async def country(country_name: str):
-    return {'country_name': country_name}
-
-@app.get('/countries/japan')
-async def japan():
-    return {'message': 'This is Japan'}
+@app.get('/countries/')
+async def country(country_name: str = 'japan', country_no: int = 1):
+    return {
+        'country_name': country_name,
+        'country_no': country_no,
+    }
